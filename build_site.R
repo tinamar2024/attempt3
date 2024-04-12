87d3de94cb20920f062d3b76da81ac3452dd2203
Ting Ma <tma@Jessicas-Air.attlocal.net>
2024-04-11 21:36:51 -0700

    attempt3 files

---
+++ build_site.R
@@ -0,0 +1,8 @@
+#Set our working directory. 
+#This helps avoid confusion if our working directory is 
+#not our site because of other projects we were 
+#working on at the time. 
+setwd("/Users/tma/Desktop/FreshR/NewDay/Basics/Web/attempt3")
+
+#render your sweet site. 
+rmarkdown::render_site()
